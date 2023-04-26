b6fe13d69a1df463aff53f9846a53c9c5ca0b77e
808 dawg <808dawgg@gmail.com>
2023-04-26 20:34:14 +0300

    Add account 2

--- src/test/java/base/RetryAnalyzer.java
+++ src/test/java/base/RetryAnalyzer.java
@@ -6,13 +6,13 @@ import org.testng.ITestResult;
 public class RetryAnalyzer implements IRetryAnalyzer {
 
     private int retryCount = 0;
-    private static final int maxRetryCount = 3;
+    private static final int maxRetryCount = 2;
 
 
     @Override
     public boolean retry(ITestResult iTestResult) {
 
-        if(retryCount <= maxRetryCount){
+        if(retryCount < maxRetryCount){
             retryCount++;
             return true;
         }
